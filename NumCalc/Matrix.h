3aaa6dd6ed91562ef3b9f106a050e101dde8e604
jdutheil <jdutheil>
2005-07-07 11:29:15 +0000

    Other bugs fixed for compatibility with gcc > 3.3.

--- NumCalc/Matrix.h
+++ NumCalc/Matrix.h
@@ -136,7 +136,7 @@ class RowMatrix : public Matrix<Scalar>, public vector< vector<Scalar> > {
 		{
 			vector< vector<Scalar> >::resize(m.nRows());	
 			for(unsigned int i = 0; i < m.nRows(); i++) {
-				operator[](i).resize(m.nCols());
+				vector< vector<Scalar> >::operator[](i).resize(m.nCols());
 				for(unsigned int j = 0; j < m.nCols(); j++) {
 					operator()(i,j) = m(i, j);
 				}
@@ -148,17 +148,17 @@ class RowMatrix : public Matrix<Scalar>, public vector< vector<Scalar> > {
 		
 		const Scalar & operator()(unsigned int i, unsigned int j) const
 		{
-			return operator[](i)[j];
+			return vector< vector<Scalar> >::operator[](i)[j];
 		}
 		
 		Scalar & operator()(unsigned int i, unsigned int j)
 		{
-			return operator[](i)[j];
+			return vector< vector<Scalar> >::operator[](i)[j];
 		}
 		
-		unsigned int nRows() const { return size(); }
+		unsigned int nRows() const { return vector< vector<Scalar> >::size(); }
 		
-		unsigned int nCols() const { return size() == 0 ? 0 : operator[](0).size(); }
+		unsigned int nCols() const { return vector< vector<Scalar> >::size() == 0 ? 0 : vector< vector<Scalar> >::operator[](0).size(); }
 		
 		vector<Scalar> row(unsigned int i) const
 		{
@@ -178,7 +178,7 @@ class RowMatrix : public Matrix<Scalar>, public vector< vector<Scalar> > {
 		{
 			vector< vector<double> >::resize(nRows);
 			for(unsigned int i = 0; i < nRows; i++) {
-				operator[](i).resize(nCols);
+				vector< vector<Scalar> >::operator[](i).resize(nCols);
 			}
 		}
 
